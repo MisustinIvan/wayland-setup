864e5ffbf1c3cdf1ba747a898055292a5c15cc17
MisustinIvan <ivanmis596@gmail.com>
2025-10-05 11:37:48 +0200

    ready, set, go!

---
+++ init.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+echo "Copying config files..."
+
+set -x
+
+NIRI_CFG_PATH="$HOME/.config/niri"
+mkdir -p "$NIRI_CFG_PATH"
+cp ./config.kdl "$NIRI_CFG_PATH"
+
+FUZZEL_CFG_PATH="$HOME/.config/fuzzel"
+mkdir -p "$FUZZEL_CFG_PATH"
+cp ./fuzzel.ini "$FUZZEL_CFG_PATH"
+
+WAYBAR_CFG_PATH="$HOME/.config/waybar"
+mkdir -p "$WAYBAR_CFG_PATH"
+cp ./config.jsonc "$WAYBAR_CFG_PATH"
+cp ./style.css "$WAYBAR_CFG_PATH"
+
+set +x
+
+echo "Config files copied successfully"
